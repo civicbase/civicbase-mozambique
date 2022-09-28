614a09a110095dd808eb5a1218efe56dbe4bc18c
Alan Mortoni <mortoni.alan@hotmail.com>
2022-09-28 19:38:51 +1000

    feat: step 5

--- src/components/Steps/Step5.tsx
+++ src/components/Steps/Step5.tsx
@@ -8,10 +8,14 @@ const Step5 = () => {
     formState: { errors },
   } = useFormContext()
 
-  const winReasons = watch('step5.winReasons')
-  const winners2017 = watch('step5.winners2017.party')
-
-  return <div css={tw`grid grid-cols-1 gap-6`}>Step 5</div>
+  return (
+    <div css={tw`grid grid-cols-1 gap-6`}>
+      So I've been thinking about the current conditions of City and how the
+      City Government has been dealing with everything and while I don't really
+      know how great they have been doing, in my personal opinion, I really like
+      them.
+    </div>
+  )
 }
 
 export default Step5
