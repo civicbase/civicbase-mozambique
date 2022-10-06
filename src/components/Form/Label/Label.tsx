978f357245856b5a14c6d11e0e2b1116a48a8edc
Alan Mortoni <mortoni.alan@hotmail.com>
2022-10-06 16:00:30 +1000

    fix: step 1

--- src/components/Form/Label/Label.tsx
+++ src/components/Form/Label/Label.tsx
@@ -1,18 +1,22 @@
+import { Caption } from 'components/Typography'
 import tw from 'twin.macro'
 
 const Label = ({
   children,
   required,
+  number,
   ...props
 }: {
   children: any
+  number?: string
   required?: boolean
 }) => {
   return (
     <label
-      css={tw`block font-semibold text-gray-900 text-sm my-1.5 dark:(text-gray-300)`}
+      css={tw`flex items-center font-semibold text-gray-900 text-sm my-1.5 dark:(text-gray-300)`}
       {...props}
     >
+      {number && <Caption css={tw`mr-3`}>{number}</Caption>}
       {children}
       {required && <label css={tw`text-red-400 ml-1`}>*</label>}
     </label>
--- src/components/Steps/Step1.tsx
+++ src/components/Steps/Step1.tsx
@@ -4,22 +4,20 @@ import Label from 'components/Form/Label'
 import Dropdown from 'components/Dropdown'
 import { Controller, useFormContext } from 'react-hook-form'
 import tw from 'twin.macro'
-import Checkbox from 'components/Form/Checkbox'
 
 const bill = () => {
   const {
     control,
     register,
-    watch,
     formState: { errors },
   } = useFormContext()
-
-  const sharedBill = watch('step1.bill.shared')
 
   return (
     <div css={tw`grid grid-cols-1 gap-6`}>
       <div>
-        <Label required>Please enter unique ID</Label>
+        <Label number="4.1" required>
+          Please enter unique ID
+        </Label>
         <Input
           {...register('step1.unique_id', { required: true })}
           error={!!errors?.step1?.unique_id}
@@ -27,86 +25,24 @@ const bill = () => {
         />
         <FieldErrorMessage name="step1.unique_id" errors={errors} />
       </div>
-      <div>
-        <label css={tw`inline-flex space-x-4 items-center select-none`}>
-          <Checkbox {...register(`step1.bill.shared`)} />
-          <span>Provided latest bill?</span>
-        </label>
-      </div>
-      {sharedBill && (
-        <>
-          <div>
-            <Label required>Month of Bill</Label>
-            <Controller
-              name="step1.bill.month"
-              control={control}
-              render={({ field }) => (
-                <Dropdown
-                  options={[
-                    'January',
-                    'February',
-                    'March',
-                    'April',
-                    'May',
-                    'June',
-                    'July',
-                    'August',
-                    'September',
-                    'October',
-                    'November',
-                    'December',
-                  ]}
-                  value={field.value}
-                  onChange={field.onChange}
-                  placeholder="Please select a month"
-                  error={!!errors?.step1?.bill?.month}
-                />
-              )}
-            />
-            <FieldErrorMessage name="step1.bill.month" errors={errors} />
-          </div>
-          <div>
-            <Label required>Record Total Consumption (in cubic meters)</Label>
-            <Input
-              {...register('step1.bill.consumption', {
-                required: true,
-                valueAsNumber: true,
-              })}
-              error={!!errors?.step1?.bill?.consumption}
-              type="number"
-            />
 
-            <FieldErrorMessage name="step1.bill.consumption" errors={errors} />
-          </div>
-          <div>
-            <Label required>Record Total Cost Incurred (meticais)</Label>
-            <Input
-              {...register('step1.bill.cost', {
-                required: true,
-                valueAsNumber: true,
-              })}
-              error={!!errors?.step1?.bill?.cost}
-              type="number"
+      <div>
+        <Label required>Language</Label>
+        <Controller
+          name="step1.language"
+          control={control}
+          render={({ field }) => (
+            <Dropdown
+              options={['English', 'Português']}
+              value={field.value}
+              onChange={field.onChange}
+              placeholder="Please select a language"
+              error={!!errors?.step1?.language}
             />
-            <FieldErrorMessage name="step1.bill.cost" errors={errors} />
-          </div>
-          <div>
-            <Label required>Record Sanitation Tax Cost (meticais)</Label>
-            <Input
-              {...register('step1.bill.sanitation_tax_cost', {
-                required: true,
-                valueAsNumber: true,
-              })}
-              error={!!errors?.step1?.bill?.sanitation_tax_cost}
-              type="number"
-            />
-            <FieldErrorMessage
-              name="step1.bill.sanitation_tax_cost"
-              errors={errors}
-            />
-          </div>
-        </>
-      )}
+          )}
+        />
+        <FieldErrorMessage name="step1.language" errors={errors} />
+      </div>
     </div>
   )
 }
